bc11bad35f27b6b004b4eb498954ee39d92a6fe1
akhilaungadh <ST10491477@vcconnect.edu.za>
2025-04-01 11:54:50 +0200

    First commit for Meal Suggestion

--- app/src/main/java/com/example/mealsuggestionappv3/MainActivity.kt
+++ app/src/main/java/com/example/mealsuggestionappv3/MainActivity.kt
@@ -24,34 +24,34 @@ class MainActivity : AppCompatActivity() {
         val buttonSuggest = findViewById<Button>(R.id.buttonSuggest)
         val buttonReset = findViewById<Button>(R.id.buttonReset)
         val textViewSuggestion = findViewById<TextView>(R.id.textViewSuggestion)
-        //Author:Zarah Bulbulia
-        //url:https://github.com/zb662000/assignmen
-//This button will show meal suggestion when a user types any of the following times of day
-        //author:Zarah Bulbulia
-        //url:https://github.com/zb662000/assignmen
-        buttonSuggest.setOnClickListener {
-            val timeOfDay = editTextTime.text.toString().trim()
-            val suggestion = when(timeOfDay.lowercase()) {
-                "morning"->"Greek yogurt with honey and granola:)"
-                "mid-morning"->"Apple slices with peanut butter:)"
-                "lunch"->"Chicken and mayo toast on brown bread:)"
-                "afternoon"->"Eggs benedict with smoked salmon:)"
-                "mid-afternoon"->"Grilled chicken salad with avocado on toast:)"
-                "dinner"->"Chickpea curry with basmati rice:)"
-                else ->"Please enter a proper time of day :("
-            }
-            textViewSuggestion.text=suggestion
-        }
-        //This will reset the text view and edit text box in which case it will clear
-        //author: Zarah bulbulia
-        //url:https://github.com/zb662000/assignmen
-        buttonReset.setOnClickListener {
-            editTextTime.text.clear()
-            textViewSuggestion.text = ""
-            //This code will now make the app fucntion and work properly when the user needs to input time of day and reset the textview
-
-        }
-
+    //Author:Zarah Bulbulia
+    //url:https://github.com/zb662000/assignmen
+    //This button will show meal suggestion when a user types any of the following times of day
+    //author:Zarah Bulbulia
+    //url:https://github.com/zb662000/assignmen
+                    buttonSuggest.setOnClickListener {
+                        val timeOfDay = editTextTime.text.toString().trim()
+                        val suggestion = when(timeOfDay.lowercase()) {
+                            "morning"->"Greek yogurt with honey and granola:)"
+                            "mid-morning"->"Apple slices with peanut butter:)"
+                            "lunch"->"Chicken and mayo toast on brown bread:)"
+                            "afternoon"->"Eggs benedict with smoked salmon:)"
+                            "mid-afternoon"->"Grilled chicken salad with avocado on toast:)"
+                            "dinner"->"Chickpea curry with basmati rice:)"
+                            else ->"Please enter a proper time of day :("
+                        }
+                        textViewSuggestion.text=suggestion
+                    }
+                    //This will reset the text view and edit text box in which case it will clear
+                //author: Zarah bulbulia
+               //url:https://github.com/zb662000/assignmen
+                buttonReset.setOnClickListener {
+                    editTextTime.text.clear()
+                    textViewSuggestion.text = ""
+                    //This code will now make the app fucntion and work properly user needs to input time of day and reset the textview
 
     }
+
+
+}
 }
